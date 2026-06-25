01a234911f6ea72c4c02cc32ece7649e29ba64d5
Bol970 <2919862+Bol970@users.noreply.github.com>
2026-06-25 19:06:27 +0300

    feat(extension): UI в стиле ChatGPT + HTML-рендер ответов

--- extension/src/main.tsx
+++ extension/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Popup } from "@/Popup";
+import { ChatPanel } from "@/ChatPanel";
 import "@/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Popup />
+    <ChatPanel />
   </React.StrictMode>
 );
